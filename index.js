5728f04b72f01add18b4a884d9f96bb6ff407c06
M. Nindra Zaka <mnindrazaka@gmail.com>
2019-10-07 13:06:43 +0700

    feat(server): specify server port using env

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.post('/', (req, res) => {
   res.json(result)
 })
 
-app.listen(3000, () => console.log('server running'))
+app.listen(process.env.PORT || 3000, () => console.log('server running'))
